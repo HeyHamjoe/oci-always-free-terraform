0308514a58831b5ef3a3a53f9b003c20c653385f
HeyHamjoe <29043882+HeyHamjoe@users.noreply.github.com>
2023-05-04 13:14:07 -0400

    Cleaned up the outputs

--- outputs.tf
+++ outputs.tf
@@ -1,34 +1,34 @@
 # Output the tenancy name.
-output "tenancy-name" {
+output "Output_1_Tenancy-Name" {
   value = data.oci_identity_tenancy.freelab_tenancy.name
 }
 
-# Output the availability domain.
-output "availability-domain" {
-  value = var.ad
+# Output the availability domains in the region.
+output "Output_2_Availability-Domains" {
+  value = data.oci_identity_availability_domains.availability_domains_in_region.availability_domains
 }
 
 # Output the compartment name.
-output "compartment-name" {
+output "Output_3_Compartment-Name" {
   value = oci_identity_compartment.freelab.name
 }
 
 # Output the public IP for x86 instances.
-output "x86_64-public-IP" {
+output "Output_4_Public-IP-x86_64" {
   value = oci_core_instance.vm_instance_x86_64.*.public_ip
 }
 
 # Output the public IP for ampere instances.
-output "ampere-public-IP" {
+output "Output_5_Public-IP-ampere" {
   value = oci_core_instance.vm_instance_ampere.*.public_ip
 }
 
-# Output the list of all active volumes in the compartment.
-output "all-active-volumes" {
-  value = data.oci_core_boot_volumes.freelab_boot_volumes.boot_volumes.*.display_name
+# Output the boot volumes.
+output "Output_6_Boot-Volumes" {
+  value = data.oci_core_boot_volume.freelab_boot_volume.*.display_name
 }
 
 # Output the block volume name.
-output "block_volume_name" {
+output "Output_7_Block-Volumes" {
   value = data.oci_core_volume.freelab_block_volume.*.display_name
 }
