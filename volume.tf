6760d45405062a39bd3a2b997323aac50991d409
HeyHamjoe <29043882+HeyHamjoe@users.noreply.github.com>
2023-05-05 09:12:04 -0400

    Fix and comment on the block volume attachment.

--- volume.tf
+++ volume.tf
@@ -9,13 +9,16 @@ resource "oci_core_volume" "freelab_block_volume" {
   is_auto_tune_enabled = true
 }
 
+
+## This is commented out for now since ampere instances are hard to create in some regions.  If enabled, it will attach to the ampere instance.
 # Attach block volume to ampere instance.
-#resource "oci_core_volume_attachment" "test_volume_attachment" {
-#  attachment_type                     = "paravirtualized"
-#  instance_id                         = oci_core_instance.vm_instance_ampere.id
-#  volume_id                           = oci_core_volume.vm_instance_freelab_block_volume.id
-#  device                              = "/dev/oracleoci/oraclevdb"
-#  display_name                        = "freelab-block-volume-attachment"
-#  is_pv_encryption_in_transit_enabled = true
-#  is_read_only                        = false
-#}
+/* resource "oci_core_volume_attachment" "ampere_volume_attachment" {
+  attachment_type                     = "paravirtualized"
+  instance_id                         = oci_core_instance.vm_instance_ampere.id
+  volume_id                           = oci_core_volume.freelab_block_volume.id
+  device                              = "/dev/oracleoci/oraclevdb"
+  display_name                        = "freelab-block-volume-attachment"
+  is_pv_encryption_in_transit_enabled = true
+  is_read_only                        = false
+}
+ */
